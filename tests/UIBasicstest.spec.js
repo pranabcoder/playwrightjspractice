76ff97102bd1f19dd5198f6da1d0fd3d7e85c750
Pranab Ghosh <pranabit6@gmail.com>
2024-03-16 23:22:13 +0530

    get title in js

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -2,4 +2,7 @@ import { test } from '@playwright/test'
 
 test('First Playwright Test', async ({ page }) => {
   await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+  // get the title of the page
+  const title = await page.title()
+  console.log(`Title of the page: ${title}`); // Title of the page: Practice Page  
 })
