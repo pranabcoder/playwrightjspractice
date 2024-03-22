af3e17179f2f079036d0d3256d182d8b50b8196a
Pranab Ghosh <pranabit6@gmail.com>
2024-03-22 21:37:04 +0530

    Check multiple elements

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('First Playwright Test', async ({ page }) => {
+  const usernamePath = page.locator('#username');
   await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
   // get the title of the page
   const title = await page.title();
   console.log(`Title of the page: ${title}`); // Title of the page: Practice Page
-  await page.locator('#username').fill('rahulshetty');
+  await usernamePath.fill('rahulshetty');
   await page.locator("[type='password']").fill('learning');
   await page.locator('#signInBtn').click();
   const unsuccessfulLoginText = await page
@@ -16,4 +17,14 @@ test('First Playwright Test', async ({ page }) => {
   expect(title).toContain('LoginPage Practise | Rahul Shetty Academy');
   // Add an assertion to check the text of the unsuccessful login message
   expect(unsuccessfulLoginText).toContain('Incorrect username/password');
+});
+test('Example - Multiple elements', async ({ page }) => {
+  const usernamePath = page.locator('#username');
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+  // get the title of the page
+  const title = await page.title();
+  console.log(`Title of the page: ${title}`); // Title of the page: Practice Page
+  await usernamePath.fill('rahulshetty');
+  await page.locator("[type='password']").fill('learning');
+  await page.locator('#signInBtn').click();
 });
