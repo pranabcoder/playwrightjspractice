49c5499aa3c224c655d1da88b8ab66a1c2db910c
Pranab Ghosh <pranabit6@gmail.com>
2024-03-23 12:25:57 +0530

    Handling Child Windows

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -103,3 +103,19 @@ test('Example - Check a link is blinking or not', async ({ page }) => {
   const allTexts = await cardTitlesPath.allTextContents();
   console.log(allTexts);
 });
+test('Example - Child Window Handling', async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const usernamePath = page.locator('#username');
+  const documentLink = page.locator('[href*="documents-request"]');
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+  const [newPage] = await Promise.all([
+    context.waitForEvent('page'),
+    documentLink.click(),
+  ]);
+  const text = await newPage.locator('.red').textContent();
+  const domaniName = text.split('@')[1].split('.')[0];
+  console.log(domaniName);
+  await usernamePath.fill(domaniName);
+  console.log(await usernamePath.textContent());
+});
